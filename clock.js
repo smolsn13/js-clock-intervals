111425d085905063be131b91f4949e10ee89075f
Scott Molsness <smolsness@gmail.com>
2018-01-26 09:46:40 -0800

    changed js slightly

--- clock.js
+++ clock.js
@@ -1,22 +1,25 @@
+
 var hourHand = document.getElementById("hour");
 var minuteHand = document.getElementById("minute");
 var secondHand = document.getElementById("second");
+//get DOM elements needed to set time
 
-function setTime() {
+var now = new Date();
+var hours = now.getHours();
+var minutes = now.getMinutes();
+var seconds = now.getSeconds();
+//get current time
 
-  var now = new Date();
-  var hours = now.getHours();
-  var minutes = now.getMinutes();
-  var seconds = now.getSeconds();
+setTime = function() {
 
   var hourRotation = (hours / 12) * 360;
   var minuteRotation = (minutes * 6) + (seconds * 6) / 60;
   var secondRotation = seconds * 6;
+//set hand image rotation based on current time
 
 hourHand.style.transform = "rotate(" + hourRotation + "deg)";
 minuteHand.style.transform = "rotate(" + minuteRotation + "deg)";
 secondHand.style.transform = "rotate(" + secondRotation + "deg)";
-
-}
+};
 
 var interval = setInterval(setTime, 1000);
